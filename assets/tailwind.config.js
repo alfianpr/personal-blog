da11d55dd371a11a574c7b1f2afb65b548b77af1
alfianpr <alfian.pratama@kalbenutritionals.com>
2023-11-13 00:17:57 +0700

    init

---
+++ assets/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      "./**/*.js",
+      "../lib/personal_website.ex",
+    ],
+    plugins: [
+      require("@tailwindcss/typography"),
+    ]
+  };
+  
--- mix.exs
+++ mix.exs
@@ -7,10 +7,17 @@ defmodule PersonalWebsite.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
       deps: deps()
     ]
   end
 
+  defp aliases() do
+    [
+      "site.build": ["build", "tailwind default --minify", "esbuild default --minify"]
+    ]
+  end
+  
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
@@ -23,6 +30,10 @@ defmodule PersonalWebsite.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:nimble_publisher, "~> 0.1.3"},
+      {:phoenix_live_view, "~> 0.18.2"},
+      {:esbuild, "~> 0.5"},
+      {:tailwind, "~> 0.1.8"}
     ]
   end
 end
